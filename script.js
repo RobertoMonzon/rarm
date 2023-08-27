5f928bb049c939db4352465569db806df8901405
RobertoMonzon <engineer.monzon@gmail.com>
2023-08-27 12:50:19 -0600

    pagina re diseñada

---
+++ script.js
@@ -0,0 +1,17 @@
+let menuIcon=document.querySelector("#menu-icon");
+let navbar= document.querySelector(".navbar");
+
+menuIcon.onclick= () =>{
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+};
+
+
+const typed = new Typed('.multiple-text',{
+    strings: ['Biomedical Engineer','Clinical application specialist','Frontend Developer','Data engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
